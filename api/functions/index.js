52e94d4a8cc0825d11ba34c0f6adccb8f0f60d57
LiramGoli <83345475+LiramGoli@users.noreply.github.com>
2023-07-22 23:29:19 +0300

    site is up!!! :)

--- api/functions/index.js
+++ api/functions/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 const serverless = require("serverless-http")
 const dotenv = require("dotenv");
-const cors = require("cors");
+const cors = require("cors"); // Import the cors package
 
 const userRouter = require("../router/user");
 const companyRouter = require("../router/company");
@@ -11,24 +11,28 @@ const authRouter = require("../router/auth");
 const teamRouter = require("../router/team");
 const productRouter = require("../router/product");
 const orderRouter = require("../router/order");
-const sanityRouter= require("../router/sanity")
+const sanityRouter = require("../router/sanity")
 
 dotenv.config();
 const app = express();
 
-//middlewares
-app.use(cors());
+// Enable CORS for all routes
+var corsOptions = {
+  origin: ["https://hashacharstorage.netlify.app","https://64bc3718c252e02581126f0d--hashacharstorage.netlify.app"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 //routes
-app.use("/.netlify/functions/index",sanityRouter)
+app.use("/.netlify/functions/index", sanityRouter)
 app.use("/.netlify/functions/index/auth", authRouter);
 app.use("/.netlify/functions/index/users", userRouter);
 app.use("/.netlify/functions/index/company", companyRouter);
 app.use("/.netlify/functions/index/products", productRouter);
 app.use("/.netlify/functions/index/orders", orderRouter);
 app.use("/.netlify/functions/index/team", teamRouter);
-
 
 module.exports.handler = serverless(app);
 
