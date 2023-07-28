374c8a99dcf2d694fc64b65a98f2a7e801210e8e
LiramGoli <83345475+LiramGoli@users.noreply.github.com>
2023-07-28 18:10:08 +0300

    ok now4

--- api/index.js
+++ api/index.js
@@ -19,7 +19,8 @@ app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "*")
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  console.log(req.headers)
   next();
 })
 
